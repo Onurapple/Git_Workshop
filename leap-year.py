a93d473f296b0af7b59fa43966ce5ee07cf9f57d
Burak <94078071+BrkKy1493@users.noreply.github.com>
2021-11-23 19:45:03 +0300

    Update leap-year.py

--- leap-year.py
+++ leap-year.py
@@ -8,7 +8,7 @@ if (year % 4) == 0:
             print("{0} is a leap year".format(year))
         else:
             print("{0} is not a leap year".format(year))
-            
+            #kjrbgşkrjng
             
             
     else:
